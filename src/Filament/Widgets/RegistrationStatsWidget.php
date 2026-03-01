198bc567b266c7e9678f445132e8f7a0cf76a204
DavidMRGaona <23616974+DavidMRGaona@users.noreply.github.com>
2026-03-01 12:40:35 +0100

    feat: update widget sort values and add view condition for PendingModerationWidget

--- src/Filament/Widgets/RegistrationStatsWidget.php
+++ src/Filament/Widgets/RegistrationStatsWidget.php
@@ -11,7 +11,7 @@ use Modules\EventRegistrations\Infrastructure\Persistence\Eloquent\Models\EventR
 
 final class RegistrationStatsWidget extends StatsOverviewWidget
 {
-    protected static ?int $sort = 3;
+    protected static ?int $sort = 5;
 
     /**
      * @return array<Stat>
